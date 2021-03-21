a51a72580078312148213684012c13807e7798e3
buckleader <jasmin28virdi@gmail.com>
2021-03-21 21:43:55 +0530

    added express rate limiter

--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,8 @@ const rateLimit = require("express-rate-limit");
 
 module.exports = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many accounts created from this IP, please try again after an hour"
   });
 
 
