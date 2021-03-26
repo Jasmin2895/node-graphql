d0464e7257a97dbc2e75adf546e536a1b5cda8b3
buckleader <jasmin28virdi@gmail.com>
2021-03-26 23:31:00 +0530

    code formatting

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,70 +1,52 @@
-const axios = require("axios")
+const axios = require('axios');
+
+const API_ENDPOINTS = {
+    COUNTRIES: 'https://restcountries.eu/rest/v2/name/',
+    EXCHANGE_RATE: 'https://api.exchangeratesapi.io/latest?base=SEK',
+};
+
 module.exports = {
     Query: {
-      countries: async (parent) => {
-        try {
-            //FIXME: using https://exchangeratesapi.io/ instead of the fixer.io
-            // const FIXER_API_KEY = "65a46a3c3c9c4a87ab07b6a72500b80d" 
-            // Send country name in the API and Add a way to limit the API.
-            console.log("parent", parent)
-          const countries = await axios.get("https://restcountries.eu/rest/v2/name/ind")
-          const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?base=SEK`)
-          let currencyList = [];
-          if(data && data.rates){
-            currencyList = Object.keys(data.rates)
-          }
-          console.log("countries", )
-          const getExachangeRate = (countryCurrency) =>{
-              console.log("countryCurrency",countryCurrency,currencyList.indexOf(countryCurrency)> -1 )
-              if(currencyList.indexOf(countryCurrency)> -1){
-                return data.rates[countryCurrency]
-              }else {
-                  return -1;
-              }
-          }
-        //   console.log("getCurrencies", data)
-          return countries.data.map(({ name, capital, region, population, currencies }) => ({
-            name,
-            capital,
-            region,
-            population,
-            currencies,
-            exchangeRate: getExachangeRate(currencies[0].code)
-          }))
-        } catch (error) {
-          throw error
-        }
-      },
-      getCountries: async(parent, {name}) => {
-        console.log("name", name)
-        try {
-          const countries = await axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-          const { data } = await axios.get(`https://api.exchangeratesapi.io/latest?base=SEK`)
-          let currencyList = [];
-          if(data && data.rates){
-            currencyList = Object.keys(data.rates)
-          }
-          console.log("countries", )
-          const getExachangeRate = (countryCurrency) =>{
-              console.log("countryCurrency",countryCurrency,currencyList.indexOf(countryCurrency)> -1 )
-              if(currencyList.indexOf(countryCurrency)> -1){
-                return data.rates[countryCurrency]
-              }else {
-                  return -1;
-              }
-          }
-        //   console.log("getCurrencies", data)
-          return countries.data.map(({ name, capital, region, population, currencies }) => ({
-            name,
-            capital,
-            region,
-            population,
-            currencies,
-            exchangeRate: getExachangeRate(currencies[0].code),
-          }))
-        }catch(error) {
-          throw error
-        }
-      }      
-    }
-  }
+        getCountries: async (parent, { name }) => {
+            try {
+                const countries = await axios.get(
+                    `${API_ENDPOINTS.COUNTRIES}${name}`,
+                );
+                const { data } = await axios.get(
+                    `${API_ENDPOINTS.EXCHANGE_RATE}`,
+                );
+                let currencyList = [];
+                if (data && data.rates) {
+                    currencyList = Object.keys(data.rates);
+                }
+                const getExachangeRate = (countryCurrency) => {
+                    if (currencyList.indexOf(countryCurrency) > -1) {
+                        return data.rates[countryCurrency];
+                    } else {
+                        return -1;
+                    }
+                };
+                return countries.data.map(
+                    ({
+                        name,
+                        capital,
+                        region,
+                        population,
+                        currencies,
+                    }) => ({
+                        name,
+                        capital,
+                        region,
+                        population,
+                        currencies,
+                        exchangeRate: getExachangeRate(
+                            currencies[0].code,
+                        ),
+                    }),
+                );
+            } catch (error) {
+                throw error;
+            }
+        },
+    },
+};
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,4 +1,4 @@
-const { gql } = require("apollo-server-express")
+const { gql } = require('apollo-server-express');
 
 module.exports = gql`
     type Countries {
@@ -16,28 +16,6 @@ module.exports = gql`
         symbol: String
     }
     type Query {
-        countries: [Countries]
         getCountries(name: String!): [Countries]
     }
-`
-
-
-// module.exports = {typeDefs}
-// module.exports = buildSchema(`
-//     type Countries {
-//         name: String
-//         capital: String
-//         region: String
-//         population: Int
-//         currencies: [Currencies!]
-//         exchangeRate: Float
-//     }
-//     type Currencies {
-//         code: String
-//         name: String
-//         symbol: String
-//     }
-//     type Query {
-//         country: [Countries]
-//     }
-// `)
+`;
